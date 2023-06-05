b9bc7ae271b9d2886143fb03ab942a6279b4d71f
Mark <mark.nyambane@student.moringaschool.com>
2023-06-05 23:15:32 +0300

    update

--- front-end/src/components/Search.jsx
+++ front-end/src/components/Search.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Search = () => {
-
-    const handleSearch = (e)=>{
-        const value = e.target.value;
-    }
+  const handleSearch = (e) => {
+    const value = e.target.value;
+    console.log(value); // Do something with the search value here
+  };
 
   return (
-   <form>
-    <input type="text" placeholder='Search' onChange={handleSearch}/>
-   </form>
-  )
-}
+    <form>
+      <input type="text" placeholder="Search" onChange={handleSearch} />
+    </form>
+  );
+};
 
-export default Search
+export default Search;
