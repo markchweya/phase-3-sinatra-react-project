f38072fe2573d57a4fb46baab283b651fc28a1a4
Mark <mark.nyambane@student.moringaschool.com>
2023-06-05 22:16:33 +0300

    update

--- db/migrate/03_remove_street_and_zip_from_contacts.rb
+++
@@ -1,7 +0,0 @@
-
-class RemoveStreetAndZipFromContacts < ActiveRecord::Migration[6.1]
-    def change
-      remove_column :addresses, :street
-      remove_column :addresses, :zip
-    end
-  end
--- db/migrate/06_change_state_to_city_in_addresses.rb
+++
@@ -1,5 +0,0 @@
-class ChangeStateToCityInAddresses < ActiveRecord::Migration[6.1]
-    def change
-      rename_column :addresses, :state, :county
-    end
-  end
